37bb6243a3197065a93e33ae3dc1cfe42a6ad78f
Linda <linda.zungu@icloud.com>
2021-02-15 14:15:11 +0200

    Initial Commit

---
+++ Notr/NotrApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotrApp.swift
+//  Notr
+//
+//  Created by Linda Zungu on 2/15/21.
+//
+
+import SwiftUI
+
+@main
+struct NotrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
